6b05dc289cb86306b3bec0c5493cc3b59519bd4f
Juanjo.figue97 <juanjo.figue97@gmail.com>
2021-12-03 22:39:55 -0500

    First_day

---
+++ Day-1-3-1.py
@@ -0,0 +1,13 @@
+#Write your code below this line 👇
+#Write your code below this line 👇
+name = input("what is your name?\n")
+print(len(name))
+
+
+print(len(input("what is your name?\n")))
+
+
+
+
+
+
---
+++ Day-1-3-exercise.py
@@ -0,0 +1,13 @@
+#Write your code below this line 👇
+#Name = input("what is #your name?\n")
+
+#print("Hello" + " " +Name)
+
+print("Hello " + input("what is your name?\n")) 
+
+
+
+
+
+
+
